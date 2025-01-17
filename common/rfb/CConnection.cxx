4e8b1b6ec9a37919c408169363ad06457a9c8525
errrorrr503 <egortit-33@yandex.ru>
2025-01-17 21:34:19 +0300

    Shift-Alt switch crutch. Proper impl TODO.

--- common/rfb/CConnection.cxx
+++ common/rfb/CConnection.cxx
@@ -1,16 +1,16 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright 2011-2019 Pierre Ossman for Cendio AB
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
@@ -757,12 +757,22 @@ void CConnection::sendKeyPress(int systemKeyCode,
 void CConnection::sendKeyRelease(int systemKeyCode)
 {
   DownMap::iterator iter;
+  static int just_unexpected_released;
 
   iter = downKeys.find(systemKeyCode);
   if (iter == downKeys.end()) {
     // These occur somewhat frequently so let's not spam them unless
     // logging is turned up.
     vlog.debug("Unexpected release of key code %d", systemKeyCode);
+    if ((systemKeyCode == 50 && just_unexpected_released == 64) ||
+        (systemKeyCode == 64 && just_unexpected_released == 50)) {
+      vlog.debug("Unexpected? Sending shift-alt!");
+      sendKeyPress(0xfffe, 0x38, XK_ISO_Next_Group);
+      sendKeyRelease(0xfffe);
+      just_unexpected_released = 0;
+    }
+    else
+      just_unexpected_released = systemKeyCode;
     return;
   }
 
